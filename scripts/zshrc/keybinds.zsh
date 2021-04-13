dcf4a659895d0a0f86d35fe1306537c1cfbd786a
S1LV3R <s1lv3r@corax.team>
2021-04-13 19:06:19 +0200

    fix keybindings

--- scripts/zshrc/keybinds.zsh
+++ scripts/zshrc/keybinds.zsh
@@ -3,9 +3,9 @@ bindkey -e                                        # emacs key bindings
 bindkey ' '       magic-space                     # do history expansion on space
 bindkey '^[[3;5~' kill-word                       # ctrl + Supr
 bindkey '^[[1;5C' forward-word                    # ctrl + ->
-bindkey '^[[C'    forward-word                    # ctrl + ->
+#bindkey '^[[C'    forward-word                    # ctrl + ->
 bindkey '^[[1;5D' backward-word                   # ctrl + <-
-bindkey '^[[D'    backward-word                   # ctrl + <-
+#bindkey '^[[D'    backward-word                   # ctrl + <-
 bindkey '^[[5~'   beginning-of-buffer-or-history  # page up
 bindkey '^[[6~'   end-of-buffer-or-history        # page down
 bindkey '^[[Z'    undo                            # shift + tab undo last action

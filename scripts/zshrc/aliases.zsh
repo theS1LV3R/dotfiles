d287ff9212915f53a593ffb34e2493bd501b7ed6
S1LV3R <s1lv3r@corax.team>
2021-05-18 12:06:34 +0200

    add various emails

--- scripts/zshrc/aliases.zsh
+++ scripts/zshrc/aliases.zsh
@@ -1,7 +1,11 @@
+if [[ -f /usr/bin/lsd ]] then;
+    alias ls='lsd --color=auto'
+fi;
+
 # some more ls aliases
 alias ll='ls -l'
 alias la='ls -A'
-alias l='ls -CF'
+alias l='ls -F'
 alias lah='ls -lah'
 
 alias open='xdg-open'
@@ -11,3 +15,26 @@ alias cfd='cloudflared'
 
 # Termbin
 alias tb='nc termbin.com 9999'
+
+alias grep='grep --color'
+
+alias c=clear
+
+# Systemctl aliases
+alias start="sudo systemctl start "
+alias restart="sudo systemctl restart "
+alias stop="sudo systemctl stop "
+alias enable="sudo systemctl enable "
+alias disable="sudo systemctl disable "
+alias mask="sudo systemctl mask "
+alias unmask="sudo systemctl unmask "
+alias status="sudo systemctl status "
+alias reload="sydo systemctl reload "
+
+wttr() {
+    curl "https://wttr.in/$1?0FmM&lang=en"
+}
+
+cheat() {
+    curl "https://cheat.sh/$1"
+}

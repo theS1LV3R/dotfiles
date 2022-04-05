1c2ae698d5881c9ee8e436cafa7690ad8b4f148e
S1LV3R <s1lv3r@corax.team>
2022-04-05 22:24:16 +0200

    cleanup and packages

--- public/install.sh
+++ public/install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-pwd
-set -x
+
 set -euo pipefail
 IFS=$'\n\t'
 
@@ -12,7 +11,7 @@ export CODESPACES=false
 echo ">>> Collecting information..."
 is_arch=$(test -f /etc/arch-release && echo true || echo false)
 is_debian=$(test -f /etc/debian_version && echo true || echo false)
-export common_packages="neovim unzip zsh tmux gcc neofetch curl net-tools"
+export common_packages="neovim unzip zsh tmux gcc neofetch curl net-tools vim"
 
 # Parse arguments
 while [[ $# -gt 0 ]]; do
@@ -93,4 +92,86 @@ else
   exit 1
 fi
 
-_install common
+plugins=$(awk '{print $1;}' "$HOME/.tool-versions" | tr '\n' ' ')
+
+IFS=' ' read -r -a plugins <<<"$plugins"
+
+for plugin in "${plugins[@]}"; do
+  asdf plugin add "$plugin" 2>/dev/null || true
+done
+
+asdf install
+
+change_shell() {
+  if [ "$(command -v zsh)" ]; then
+    echo "Changing shell to zsh"
+
+    command -v zsh | sudo tee -a /etc/shells
+    user=$USER
+    sudo chsh -s "$(which zsh)" "$user"
+  fi
+}
+
+install_ptsh() {
+  orig_dir=$PWD
+
+  dir=$(mktemp -d)
+  cd "$dir"
+
+  git clone https://github.com/jszczerbinsky/ptSh .
+
+  make
+  sudo make install
+  cd "$orig_dir"
+}
+
+install_tty-clock() {
+  orig_dir=$PWD
+
+  dir=$(mktemp -d)
+
+  git clone https://github.com/xorg62/tty-clock "$dir"
+  cp -r ../misc/patches/tty-clock "$dir"/patches
+
+  cd "$dir"
+
+  patch -p1 <patches/*.patch
+
+  make
+
+  PREFIX=~/.local make install
+}
+
+pwfeedback() {
+  sudo cp ../misc/01-pwfeedback /etc/sudoers.d/
+}
+
+read -r -p ">>> Install all or ask manually? [A/m] " response </dev/tty
+if [[ ! $response =~ ^([mM])$ ]] || [[ $YES == "true" ]]; then
+  change_shell
+  install_ptsh
+  pwfeedback
+  install_tty-clock
+else
+
+  read -r -p "Set zsh as default shell? [y/N] " response </dev/tty
+  if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    change_shell
+  fi
+
+  read -r -p "Install ptsh? [y/N] " response </dev/tty
+  if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    install_ptsh
+  fi
+
+  read -r -p "Install pwfeedback to sudo? [y/N] " response </dev/tty
+  if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    pwfeedback
+  fi
+
+  read -r -p "Install tty-clock? [y/N] " response </dev/tty
+  if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    install_tty-clock
+  fi
+
+fi

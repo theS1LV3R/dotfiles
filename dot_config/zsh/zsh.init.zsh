707b325f8839e5f1310fa64519d625b00045d0d4
theS1LV3R <s1lv3r@corax.team>
2022-06-04 16:13:23 +0200

    shite

--- dot_config/zsh/zsh.init.zsh
+++ dot_config/zsh/zsh.init.zsh
@@ -4,6 +4,7 @@
 setopt autocd              # change directory just by typing its name
 setopt correct             # auto correct mistakes
 setopt interactivecomments # allow comments in interactive mode
+setopt extended_history
 setopt share_history       # share history between all sessions
 setopt magicequalsubst     # enable filename expansion for arguments of the form ‘anything=expression’
 setopt nonomatch           # hide error message if there is no match for the pattern
@@ -11,6 +12,7 @@ setopt notify              # report the status of background jobs immediately
 setopt numericglobsort     # sort filenames numerically when it makes sense
 setopt promptsubst         # enable command substitution in prompt
 setopt no_beep             # don't beep on errors
+setopt C_BASES             # 0xFF instead of 16#FF
 
 #! GENERATES ERRORS - DO NOT USE
 # setopt ksharrays           # arrays start at 0

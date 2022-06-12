841c455a08d0c6eb6f9427700f0a3d19875b5790
theS1LV3R <s1lv3r@corax.team>
2022-06-12 23:09:23 +0200

    move to zi

--- dot_config/zsh/zsh.init.zsh
+++ dot_config/zsh/zsh.init.zsh
@@ -1,10 +1,11 @@
 # Originally sourced from zshrc file
 # Everything is sourced in the order it was in the original dotfile
 
+setopt AUTO_PUSHD          # automatically push to the directory stack
 setopt autocd              # change directory just by typing its name
 setopt correct             # auto correct mistakes
 setopt interactivecomments # allow comments in interactive mode
-setopt extended_history
+setopt extended_history    # include timestamps in history
 setopt share_history       # share history between all sessions
 setopt magicequalsubst     # enable filename expansion for arguments of the form ‘anything=expression’
 setopt nonomatch           # hide error message if there is no match for the pattern
@@ -13,6 +14,7 @@ setopt numericglobsort     # sort filenames numerically when it makes sense
 setopt promptsubst         # enable command substitution in prompt
 setopt no_beep             # don't beep on errors
 setopt C_BASES             # 0xFF instead of 16#FF
+setopt COMPLETE_ALIASES
 
 #! GENERATES ERRORS - DO NOT USE
 # setopt ksharrays           # arrays start at 0
--- dot_config/zsh/zshrc/completion.zsh
+++ dot_config/zsh/zshrc/completion.zsh
@@ -1,8 +1,3 @@
-# enable completion features (see under)
-autoload -Uzd bashcompinit && bashcompinit
-# compinit -d ~/.cache/zcompdump
 complete -C '/usr/bin/aws_completer' aws
 
-# tabtab source for netlify and pnpm
-# uninstall by removing these lines
 [[ -f ~/.config/tabtab/__tabtab.zsh ]] && . ~/.config/tabtab/__tabtab.zsh

1015c811f32a7207c598150cacad5d60df12e73a
theS1LV3R <me@s1lv3r.codes>
2023-05-26 20:01:55 +0200

    fix alacritty themes script

--- dot_config/alacritty/themes/run_onchange_themes.sh
+++ dot_config/alacritty/themes/run_onchange_themes.sh
@@ -2,6 +2,7 @@
 
 TMPDIR=$(mktemp -d)
 REPO="https://github.com/alacritty/alacritty-theme"
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 
 mkdir -p "$XDG_CONFIG_HOME/alacritty/themes"
 git clone --depth 1 "$REPO" "$TMPDIR"

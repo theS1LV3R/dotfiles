a4b0774ba0ae5ccd759f947a9b947aee51770609
theS1LV3R <me@s1lv3r.codes>
2025-01-02 00:08:51 +0100

    Verify device exists before trying to use it

--- dot_local/bin/executable_setinput.sh
+++ dot_local/bin/executable_setinput.sh
@@ -5,7 +5,6 @@
 # shellcheck source=executable___common.sh
 source "$HOME/.local/bin/__common.sh"
 
-
 set -o errexit                  # exit on error
 set -o pipefail                 # fail early in piped commands
 set -o nounset                  # do not use unset variables
@@ -24,23 +23,41 @@ now() { date "+%Y-%m-%d %H:%M:%S"; }
 get_device_id() {
     local device_name="$1"
 
-    pactl list sources \
-        | grep -e "Source #" -e "Description:" \
-        | grep -iv monitor \
-        | grep -iB1 "$device_name" \
-        | head -n1 \
-        | awk '{print $2}' \
-        | tr -d '#'
+    pactl list sources |
+        grep -e "Source #" -e "Description:" |
+        grep -iv monitor |
+        grep -iB1 "$device_name" |
+        head -n1 |
+        awk '{print $2}' |
+        tr -d '#'
+}
+
+list_device_names() {
+    log_info "Valid device names:"
+    pactl list sources |
+        grep -e "Description:" |
+        grep -iv "monitor" |
+        cut -c 15-
 }
 
 main() {
     local device
     local device_id
 
+    if [[ ${#@} -lt 1 ]]; then
+        log_error "Missing required param \$1 for device name"
+        list_device_names
+        exit 1
+    fi
+
     device="$1"
     check_dependencies "${dependencies[@]}"
 
-    device_id=$(get_device_id "$device")
+    device_id=$(get_device_id "$device" || true)
+    if [[ -z "$device_id" ]]; then
+        list_device_names
+        exit 1
+    fi
     log_info "Resolved device id for '$device': $device_id"
 
     pactl set-default-source "$device_id"
